df9ea8a54c0ae27e0f7d334397df31a23eda4da3
Petr <petr.kurapov@gmail.com>
2021-04-08 21:32:25 +0300

    Add python finder

---
+++ cmake/hello.py
@@ -0,0 +1,4 @@
+import sys
+print("Hello world from Python " + str(sys.version) + " " + str(sys.version_info) + "\n")
+#with open('result.txt', 'w') as f:
+#    f.write("hello world from " + str(sys.version) + " " + str(sys.version_info) + "\n")
